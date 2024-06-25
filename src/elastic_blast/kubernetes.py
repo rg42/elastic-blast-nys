03830d7ed1c1dfd9d93d4c236608b307303117e6
rg42 <rg42@users.noreply.github.com>
2024-06-24 19:03:43 -0500

    showing actual error message as well

--- src/elastic_blast/kubernetes.py
+++ src/elastic_blast/kubernetes.py
@@ -882,9 +882,10 @@ def enable_service_account(cfg: ElasticBlastConfig):
         else:
             try:
                 safe_exec(cmd)
-            except:
+            except Exception as e:
                 msg = 'ElasticBLAST is missing permissions for its auto-shutdown and cloud job submission feature. To provide these permissions, please run '
                 msg += f'gcloud projects add-iam-policy-binding {cfg.gcp.project} --member={cfg.gcp.user} --role=roles/container.admin'
+                msg += f'Actual error: {str(e)}'
                 raise UserReportError(returncode=PERMISSIONS_ERROR, message=msg)
 
 
